ee981b92b6af559073e3fc60b639a17acb161ed5
thegreatlizzyator <barthelemy.elizabeth@gmail.com>
2020-03-03 15:43:23 +0100

    created setup.py for packaging

--- setup.py
+++ setup.py
@@ -1,29 +1,22 @@
-# -*- coding: utf-8 -*-
-"""
-find cool name.
-
-"The unpredictable and the predetermined unfold together to make everything the way it is.
-It’s how nature creates itself, on every scale, the snowflake and the snowstorm."
-Tom Stoppard, Arcadia (Faber and Faber, London, 1993)
-
-@author: Elizabeth Barthelemy
-"""
-
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
 
-with open('README.md') as f:
-    readme = f.read()
-
-with open('LICENSE.txt') as f:
-    license = f.read()
-
-setup(name='findcoolname',
-      version='0.1',
-      description='Description',
-      url='http://github.com/thegreatlizzyator/findcoolname',
-      author='Elizabeth Barthelemy',
-      author_email='elizabeth.barthelemy@univ-grenoble-alpes.fr',
-      license='CeCILL',
-      packages=find_packages(exclude=('tests', 'docs')),
-      zip_safe=False)
+setup(
+    name="ecophylo",
+    version="0.0.1",
+    url="https://github.com/thegreatlizzyator/ecophylo",
+    download_url="https://github.com/thegreatlizzyator/ecophylo/archive/master.zip",
+    license="CeCILL",
+    author="Elizabeth Barthelemy",
+    author_email="elizabeth.barthelemy@univ-grenoble-alpes.fr",
+    description="Coalescent-based simulations of eco-evolutionary biodiverity dynamics",
+    long_description=readme,,
+    packages=find_packages(exclude=("tests",)),
+    install_requires=["setuptools", "requests"],
+    classifiers=[
+	"Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: CeCILL FREE SOFTWARE LICENSE AGREEMENT",
+    ],
+)
