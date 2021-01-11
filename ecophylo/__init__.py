7e222e99aa9279e6fb1e3512c8a07618fca2c5fa
gowachin <gowachin974@gmail.com>
2021-01-11 12:13:55 +0100

    Rmd with retiulate working and idiotproof

--- ecophylo/__init__.py
+++ ecophylo/__init__.py
@@ -1,7 +1,14 @@
 from ecophylo.dosimulate import dosimuls
+from ecophylo.dosimulate import simulate
+from ecophylo.dosimulate import params
+from ecophylo.dosimulate import getSFS
 
-import sys
-import msprime
-import numpy as np
-from ete3 import Tree
-import pandas as pd
+from ecophylo.pastdemo import timeframes
+from ecophylo.pastdemo import demographic_events
+
+from ecophylo.islmodel import population_configurations
+from ecophylo.islmodel import migration_matrix
+from ecophylo.islmodel import mass_migrations
+
+from ecophylo.toPhylo import toPhylo
+from ecophylo.toPhylo import ubranch_mutation
