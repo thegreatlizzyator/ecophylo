53f5b32f303b06f60f0739aa97d67106bf1dcb12
gowachin <gowachin974@gmail.com>
2021-01-21 17:10:34 +0100

    getSFS -> getAbund

--- ecophylo/__init__.py
+++ ecophylo/__init__.py
@@ -1,7 +1,7 @@
 from ecophylo.dosimulate import dosimuls
 from ecophylo.dosimulate import simulate
 from ecophylo.dosimulate import params
-from ecophylo.dosimulate import getSFS
+from ecophylo.dosimulate import getAbund
 from ecophylo.dosimulate import sample
 
 from ecophylo.pastdemo import timeframes
