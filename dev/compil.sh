84e78cac98aa851df82dc765e2fb43a817e6c90f
gowachin <gowachin974@gmail.com>
2021-03-09 14:28:51 +0100

    sizes2rates & mergesizes2rates function added

--- dev/compil.sh
+++ dev/compil.sh
@@ -1,4 +1,7 @@
 rm dist/*
 python3 setup.py sdist bdist_wheel
-python3 -m pip install dist/ecophylo-0.0.6.tar.gz
+python3 -m pip install dist/ecophylo-0.0.7.tar.gz
+printf "\n=========================================================================\n"
+printf "=============================  TESTING   ================================\n"
+printf "=========================================================================\n"
 python3 test.py
