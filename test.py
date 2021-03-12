ed95e16c274fecdf23658e050315f3be30139681
gowachin <gowachin974@gmail.com>
2021-03-12 17:20:40 +0100

    testing merge2sizes2rates

--- test.py
+++ test.py
@@ -11,27 +11,30 @@ if __name__ == "__main__":
     print("phylogen examples")
     print(doctest.testmod(phylogen))
     print('\nphylogen error tests')
-    print(doctest.testfile("tests/test-toPhylo.txt"))
-    print(doctest.testfile("tests/test-ubranch_mutation.txt"))
+    print('toPhylo :', doctest.testfile("tests/test-toPhylo.txt"))
+    print('ubranch_mutation :', doctest.testfile("tests/test-ubranch_mutation.txt"))
 
 
     print('\n==================')
     print("islmodel examples")
     print(doctest.testmod(islmodel))
     print('\nislmodel error tests')
-    print(doctest.testfile("tests/test-sizes2rates.txt"))
-    
+    print('sizes2rates :', doctest.testfile("tests/test-sizes2rates.txt"))
+    print('mergesizes2rates :', doctest.testfile("tests/test-mergesizes2rates.txt"))
+
+
     print('\n==================')
     print('Pastdemos examples')
     print(doctest.testmod(pastdemo))
     print('\nPastdemos error tests')
-    print(doctest.testfile("tests/test-timeframes.txt"))
-    print(doctest.testfile("tests/test-demographic_events.txt"))
-    
+    print('timeframes :', doctest.testfile("tests/test-timeframes.txt"))
+    print('demographic_events :', doctest.testfile("tests/test-demographic_events.txt"))
+
+
     print('\n==================')
     print('Dosimulate examples')
     print(doctest.testmod(dosimulate))
     print('\nDosimulate error tests')
-    print(doctest.testfile("tests/test-getAbund.txt"))
+    print('getAbund :', doctest.testfile("tests/test-getAbund.txt"))
 
 
