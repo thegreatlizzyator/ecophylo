42b19191f45248aa2513edc48d41ba1a929e71a6
gowachin <gowachin974@gmail.com>
2021-03-16 17:15:57 +0100

    tree print is now ascii because windows is shitty with unicode...wont even comment this!

--- dev/compil.sh
+++ dev/compil.sh
@@ -4,8 +4,8 @@ printf "\n======================================================================
 printf "=============================  Install   ================================\n"
 printf "=========================================================================\n"
 printf "installing without internet. will crash if change in dependencies\n"
-python3 -m pip install dist/ecophylo-0.0.7.tar.gz --no-deps --no-index
-#python3 -m pip install dist/ecophylo-0.0.7.tar.gz 
+python3 -m pip install dist/ecophylo-0.0.8.tar.gz --no-deps --no-index
+#python3 -m pip install dist/ecophylo-0.0.8.tar.gz 
 printf "\n=========================================================================\n"
 printf "=============================  TESTING   ================================\n"
 printf "=========================================================================\n"
