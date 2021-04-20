63fb16a0b2f3c01d3cef19506ba196c834af85af
gowachin <gowachin974@gmail.com>
2021-04-20 16:40:53 +0200

    adding migration to new simulate_dolly

--- dev/build_demography.py
+++ dev/build_demography.py
@@ -6,6 +6,8 @@ samples= [5, 5]
 comsizes = [[10,20],[40,50,60]]
 changetimes = [[0,100], [0,300,400]]
 
+
+
 migr = [[[0,0.1],
         [0.2,0]]
         ,
@@ -13,9 +15,19 @@ migr = [[[0,0.1],
         [0.4,0]]
         ,
         [[0,0.5],
+        [0.6,0]]
+        ,
+        [[0,0.7],
         [0.6,0]]]
-migr = [1, 0.5, 0.8] 
-changemigr = [0, 100, 200]
+
+migr = [1, 0.5, 0.8, 0.7]
+
+test = [isinstance(i, (float,int)) for i in migr]
+print(test)
+print(sum(test) == len(test))
+
+
+changemigr = [0, 100, 200, 300]
 
 rates = [[0,-0.002], [0,0]]
 changerates = [[0,100], [600, 80]]
@@ -24,19 +36,46 @@ changerates = [[0,100], [600, 80]]
 if True :
     """
     """
-    
-    # IDIOT PROOF HERE
-    # check that there are as many changetimes[pop] as comsize[pop]
-     
-
     npop = len(samples)
-    pop_ids = ["pop_" + str(p) for p in range(npop)]
 
-    demography = msprime.Demography()
+    # migration idiot proof
+    if migr is not None:
+        if npop == 1 :
+            sys.warn("no migration matrix is needed for a single deme")
+            migr = None
+        m = np.array(migr)
+        dim = m.shape
+
+        if np.sum(m) == 0 :
+            sys.exit("migration matrices cannot all be empty")
+
+        if dim[0] != len(changemigr):
+            sys.exit("there should be as many symmetric migration rates as there are times in changemigr")
+        if len(dim) > 1:
+            if dim[1] != dim[2] or dim[1] != npop or dim[2] != npop:
+                sys.exit("custom migration matrices should be of size ndeme x ndeme")
+            for mat in migr:
+                isint_migr = [isinstance(i, (float,int)) for i in mat]
+                ispos_migr = [i>=0 or i<= npop for i in mat]
+                if not all(isint_migr) :
+                    sys.exit("found custom migration matrix that is not made of ints or floats")
+                if not all(ispos_migr):
+                    sys.exit("found custom migration matrix with negative migration rates or greater than the number of demes")
+        else:
+            isint_migr = [isinstance(i, (float,int)) for i in migr]
+            ispos_migr = [i>=0 or i<= npop for i in migr]
+            if not all(isint_migr):
+                sys.error("migration rates should be either ints or floats")
+            if not all(ispos_migr): 
+                sys.error("migration rate should be positive (or zero) and not exceed the number of demes")
     
+
+    pop_ids = ["pop_" + str(p) for p in range(npop)]
+    demography = msprime.Demography()
+
     # build SampleSet
     sampleset = [msprime.SampleSet(samp, pop, ploidy=1) for samp,pop in zip(samples, pop_ids)]
-
+    
     # initialize population configurations
     for pop in range(npop):
         demography.add_population(initial_size= comsizes[pop][0], growth_rate=rates[pop][0])
@@ -53,11 +92,6 @@ if True :
 
     # initialize migration matrix    
     if migr is not None:
-        m = np.array(migr)
-        dim = m.shape
-        if np.sum(m) == 0 :
-            print("ERROR: migration matrices cannot all be empty")
-
         if len(dim) == 1 :
             # symmetric migration matrix
             demography.set_symmetric_migration_rate(populations = range(npop), rate = migr[0])
@@ -80,14 +114,14 @@ if True :
                 for t in range(len(changemigr)):
                     for row in range(npop):
                         for col in range(npop):
-                            if migr[t+1][row][col] == 0 :
+                            if migr[t][row][col] == 0 :
                                 continue
-                            demography.add_migration_rate_change(time = changemigr[t], rate = migr[t+1][row][col], source=pop_ids[row], dest=pop_ids[col])
+                            demography.add_migration_rate_change(time = changemigr[t], rate = migr[t][row][col], source=pop_ids[row], dest=pop_ids[col])
 
 
 
 demography.sort_events()
-print(demography.debug())
+#print(demography.debug())
 #ts = msprime.sim_ancestry(demography = demography, samples = sampleset)
 #tree = ts.first()
 #print(tree.draw(format = "unicode"))
