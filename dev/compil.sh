9e0a8fd261f5af012591288e64bd2b8da8c24d7d
gowachin <gowachin974@gmail.com>
2021-04-29 17:14:16 +0200

    0.1.0 version

--- dev/compil.sh
+++ dev/compil.sh
@@ -4,8 +4,8 @@ printf "\n======================================================================
 printf "=============================  Install   ================================\n"
 printf "=========================================================================\n"
 printf "installing without internet. will crash if change in dependencies\n"
-python3.7 -m pip install dist/ecophylo-0.0.9.tar.gz --no-deps --no-index
-#python3 -m pip install dist/ecophylo-0.0.9.tar.gz 
+python3.7 -m pip install dist/ecophylo-0.1.0.tar.gz --no-deps --no-index
+#python3 -m pip install dist/ecophylo-0.1.0.tar.gz 
 printf "\n=========================================================================\n"
 printf "=============================  TESTING   ================================\n"
 printf "=========================================================================\n"
